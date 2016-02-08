6fc74696562a760616618dc9c99165dd92edaf1f
weslylim94 <weslylim94@gmail.com>
2016-02-07 20:25:54 -0800

    email works

--- controllers/default.py
+++ controllers/default.py
@@ -1,5 +1,9 @@
 from gluon.tools import Mail
-from gluon.contrib.login_methods.email_auth import email_auth
+mail = Mail()
+
+mail.settings.server = 'smtp.gmail.com:587'
+mail.settings.sender = 'dontforgetyourevent@gmail.com'
+mail.settings.login = 'dontforgetyourevent@gmail.com:web2pyucsc'
 
 
 def user():
@@ -7,10 +11,18 @@ def user():
 
 def index():
     alarms = db().select(db.alarm.ALL, orderby=db.alarm.name)
-    form=FORM('Your name:', INPUT(_name='name'), INPUT(_type='redirect'))    
+    form=FORM('Your name:', INPUT(_name='name'), INPUT(_type='redirect'))
+    if mail:
+        if mail.send(to=['weslylim94@gmail.com'],
+                subject='test',
+                message= 'test'
+            ):
+                response.flash = 'email sent sucessfully.'
+        else:
+                response.flash = 'fail to send email sorry!'
+    else:
+            response.flash = 'Unable to send the email : email parameters not defined'
     return dict(alarms=alarms, form=form)
-
-
 
 def create():
 
@@ -22,30 +34,5 @@ def create():
 def show():
     alarms = db.alarm(request.args[0]) or redirect(URL('index'))
 
-    mail = Mail()
-    ##mail = auth.settings.mailer
-    ##mail.settings.tls=True
-    mail.settings.server = 'http://smtp.webfaction.com:25'
-    mail.settings.sender = 'web2py@cmps183.webfactional.com'
-    mail.settings.login = '183mailbox:web2pyucsc'
-
-
-
-    att = "@txt.att.net"
-    theEmail = alarms.number + att
-    if mail:
-        x = mail.send(to=['gvelazq3@ucsc.edu'],
-            subject='ALARM!',
-            message= "YOUR ALARM!AHHHHHHH!LAJDSLJA")
-        if x == True:
-            response.flash = 'email sent sucessfully.'
-        else:
-            response.flash = 'fail to send email sorry!'
-    else:
-        response.flash = 'Unable to send the email'
-        
-    return dict(alarm=alarms, message=theEmail,form=auth.register())
-
 def init():
     return dict(message="Hello")
-
