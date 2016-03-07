b33b7343cd19bb457fbe114c01179dfb8429a229
talal.abouhaiba <talal.abouhaiba@gmail.com>
2016-03-06 20:52:45 -0800

    Mailing script now works correctly, sends emails

--- private/mailer.py
+++ private/mailer.py
@@ -2,6 +2,13 @@
 
 import time
 from datetime import datetime
+from gluon.tools import Mail
+
+mail = Mail()
+
+mail.settings.server = 'smtp.gmail.com:587'
+mail.settings.sender = 'dontforgetyourevent@gmail.com'
+mail.settings.login = 'dontforgetyourevent@gmail.com:web2pyucsc'
 
 def phoneProviderList(phonenumber):
 
@@ -16,29 +23,35 @@ def phoneProviderList(phonenumber):
     listOfNumbers.append(phonenumber + "@vtext.com")                #Verizon
     return listOfNumbers
 
-while True: # inf loop 
+while True: # inf loop
 
     currentMin = datetime.now().minute
     currentHour = datetime.now().hour
+
+    if currentHour < 10:
+    	currentHourString = "0" + str(currentHour)
+    else:
+    	currentHourString = str(currentHour)
 
     # Current Date YYYY-MM-DD
 
     currentDate = datetime.now().date
 
     print(":re")
-    
+
     # Current Time HH:MM:00
 
-    currentTime = str(currentHour) + ":" + str(currentMin) + ":00"
+
+    currentTime = currentHourString + ":" + str(currentMin) + ":00"
+
+    print(currentTime)
 
     rows = db((db.alarm.reminder_date == currentDate) & (db.alarm.reminder_time==currentTime)).select()
 
     for row in rows:
         theList = phoneProviderList(row.phone_number)
-
         print(row.phone_number + " -- " + str(row.reminder_date) + " -- " + str(row.reminder_time) + " -- " + row.reminder_message)
 
-        # mail.send(to=theList,subject="Don't Forget!",message=row.reminder_message)
+        mail.send(to=theList,subject="Don't Forget!",message=row.reminder_message)
 
-    time.sleep(15) # check every 60s
-
+    time.sleep(50) # check every 50s
