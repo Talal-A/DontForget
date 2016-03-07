71c06d5b7866ff7764b95ed25584703d1a61411a
talal.abouhaiba <talal.abouhaiba@gmail.com>
2016-03-06 19:55:59 -0800

    Updated mailer script.
    Currently checks for both date AND time.
    Doesn't email, just prints to console.

--- private/mailer.py
+++ private/mailer.py
@@ -3,20 +3,42 @@
 import time
 from datetime import datetime
 
+def phoneProviderList(phonenumber):
+
+    listOfNumbers = []
+    listOfNumbers.append(phonenumber + "@text.wireless.alltel.com") #Alltel
+    listOfNumbers.append(phonenumber + "@text.att.net")             #AT&T
+    listOfNumbers.append(phonenumber + "@sms.mycricket.com")        #Cricket
+    listOfNumbers.append(phonenumber + "@messaging.sprintpcs.com")  #Sprint
+    listOfNumbers.append(phonenumber + "@page.nextel.com")          #Nextel
+    listOfNumbers.append(phonenumber + "@tmomail.net")              #T-Mobile
+    listOfNumbers.append(phonenumber + "@email.uscc.net")           #U.S. Cellular
+    listOfNumbers.append(phonenumber + "@vtext.com")                #Verizon
+    return listOfNumbers
+
 while True: # inf loop 
 
     currentMin = datetime.now().minute
     currentHour = datetime.now().hour
 
+    # Current Date YYYY-MM-DD
+
+    currentDate = datetime.now().date
+
+    print(":re")
+    
+    # Current Time HH:MM:00
+
     currentTime = str(currentHour) + ":" + str(currentMin) + ":00"
 
-    rows = db(db.alarm.reminder_time==currentTime).select()
+    rows = db((db.alarm.reminder_date == currentDate) & (db.alarm.reminder_time==currentTime)).select()
 
     for row in rows:
         theList = phoneProviderList(row.phone_number)
 
-        mail.send(to=theList,
-             subject="Don't Forget!",
-             message=row.reminder_message)
+        print(row.phone_number + " -- " + str(row.reminder_date) + " -- " + str(row.reminder_time) + " -- " + row.reminder_message)
 
-    time.sleep(60) # check every 60s
+        # mail.send(to=theList,subject="Don't Forget!",message=row.reminder_message)
+
+    time.sleep(15) # check every 60s
+
